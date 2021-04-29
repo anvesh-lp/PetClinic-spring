1bc27020e2ded1a01e0dba5011436c466dda05f0
Naga Anvesh <knanvesh.01@gmail.com>
2021-04-29 23:12:43 +0530

    List of Owners

--- Pet_Clinic_Web/src/main/java/com/Anvesh/petclinicspring/controllers/OwnersController.java
+++ Pet_Clinic_Web/src/main/java/com/Anvesh/petclinicspring/controllers/OwnersController.java
@@ -1,15 +1,23 @@
 package com.Anvesh.petclinicspring.controllers;
 
 
+import com.Anvesh.petclinicspring.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/owners"})
 @Controller
 public class OwnersController {
+    private final OwnerService service;
+
+    public OwnersController(OwnerService service) {
+        this.service = service;
+    }
 
     @RequestMapping({"", "/owners"})
-    public String ownerList() {
+    public String ownerList(Model model) {
+        model.addAttribute("owners", service.findAll());
         return "owners/ownersList";
     }
 }
