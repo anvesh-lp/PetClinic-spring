e6c0757eb27d952013a6e5c5c3c5ebf3beed623b
Naga Anvesh <knanvesh.01@gmail.com>
2021-05-01 12:42:38 +0530

    Adding Contact details to owner

--- Pet_Clinic_Data/src/main/java/com/Anvesh/petclinicspring/model/Owner.java
+++ Pet_Clinic_Data/src/main/java/com/Anvesh/petclinicspring/model/Owner.java
@@ -4,6 +4,15 @@ import java.util.Set;
 
 public class Owner extends Person {
     private Set<Pet> pets;
+    private Contact contact;
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
 
     public Set<Pet> getPets() {
         return pets;
