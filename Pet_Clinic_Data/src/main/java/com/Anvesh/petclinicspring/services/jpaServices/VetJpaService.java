bb97298164a3c16b7628ce95c7a49c07a1473c05
Naga Anvesh <knanvesh.01@gmail.com>
2021-05-05 21:01:05 +0530

    Implementing VetJpaRepository

---
+++ Pet_Clinic_Data/src/main/java/com/Anvesh/petclinicspring/services/jpaServices/VetJpaService.java
@@ -0,0 +1,45 @@
+package com.Anvesh.petclinicspring.services.jpaServices;
+
+import com.Anvesh.petclinicspring.model.Vet;
+import com.Anvesh.petclinicspring.repositories.VetRepository;
+import com.Anvesh.petclinicspring.services.VetService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class VetJpaService implements VetService {
+    private final VetRepository repository;
+
+    public VetJpaService(VetRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        repository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return repository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return repository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        repository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        repository.deleteById(aLong);
+    }
+}
