aed262f53af0a96f4e583218d4b13b4685614e09
Naga Anvesh <knanvesh.01@gmail.com>
2021-05-05 16:44:26 +0530

    Mapping JPA entities to visits and petw

--- Pet_Clinic_Data/src/main/java/com/Anvesh/petclinicspring/model/Pet.java
+++ Pet_Clinic_Data/src/main/java/com/Anvesh/petclinicspring/model/Pet.java
@@ -3,7 +3,10 @@ package com.Anvesh.petclinicspring.model;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Pet extends BaseEntity {
@@ -13,6 +16,8 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
     private LocalDate date;
+    @OneToMany
+    private Set<Visit> visitSet = new HashSet<>();
 
     public PetType getPetType() {
         return petType;
@@ -36,6 +41,14 @@ public class Pet extends BaseEntity {
 
     public void setDate(LocalDate date) {
         this.date = date;
+    }
+
+    public Set<Visit> getVisitSet() {
+        return visitSet;
+    }
+
+    public void setVisitSet(Set<Visit> visitSet) {
+        this.visitSet = visitSet;
     }
 
     @Override
